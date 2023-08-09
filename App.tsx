306ea3888b5d17a6332b869d8ff822a8897dc228
shankar-balasubramaniam <shankarb3061997@gmail.com>
2023-08-09 15:35:08 +0530

    add Provider missed in prev commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
+import {Provider} from 'react-redux';
 import React from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
+import store from './src/rtk/store';
 
 const App = () => {
-  return <RootNavigator />;
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
 };
 
 export default App;
